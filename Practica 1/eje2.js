8d74d153339bd0dc5dff1cd9dd3ecf99c75810c3
angel-leon-solis <124051489@upq.edu.mx>
2025-09-19 20:36:01 -0600

    paso 2 (bueno)
    se cambio la función tradicional a una arrow y se corrío en consola con 3 números diferentes

---
+++ Practica 1/eje2.js
@@ -0,0 +1,8 @@
+function cuadrado(numero){
+    return numero * numero;
+}
+
+const cuadradoo = numero => numero * numero;
+console.log(cuadradoo(2));
+console.log(cuadradoo(7));
+console.log(cuadradoo(4));
