7898ee511fe376352b18a7d0eaf99580f3d9221a
angel-leon-solis <124051489@upq.edu.mx>
2025-09-19 20:33:31 -0600

    paso 1 de la practica(el bueno)
    se hizo un cambio de variables de nombre y edad como let ya que estas si pueden cambiar y la de saludo como const ya que el mensaje que da siempre tiene que dar ese mensaje

---
+++ Practica 1/eje1.js
@@ -0,0 +1,12 @@
+let nombre = "angel";
+let edad = 25;
+
+const saludo = "hola " + nombre + "tienes " + edad + "años";
+console.log(saludo);
+
+
+//ejercicio 2
+function cuadrado(numero){
+    return numero * numero;
+}
+console.log
