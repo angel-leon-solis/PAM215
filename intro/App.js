8bde0795b52df6a879bf2dcec3da7f37217be250
angel-leon-solis <124051489@upq.edu.mx>
2025-10-10 20:18:00 -0600

    practica 5
    se realizo la primer pantalla donde se puso botones de incrementar decrementar y reiniciar

--- intro/App.js
+++ intro/App.js
@@ -1,15 +1,27 @@
+//1. impots: zona de importaciones 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Button , } from 'react-native';
+import React,{useState} from 'react';
 
+//2. impots: zona de componentes
 export default function App() {
+
+  const[contador,setcontador]=useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Hola mundo</Text>
+      <Text>contador:{contador}</Text>
+      <Button title='incrementar' onPress={()=>setcontador(contador+1)}> </Button>
+      <Button title='decrementar' onPress={()=>setcontador(contador-1)}> </Button>
+      <Button title='reiniciar' onPress={()=>setcontador(0)}> </Button>
       <StatusBar style="auto" />
     </View>
-  );
-}
 
+
+  );
+ }
+
+//3. impots: zona de estilos y posicionamiento
 const styles = StyleSheet.create({
   container: {
     flex: 1,
